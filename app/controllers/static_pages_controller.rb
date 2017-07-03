6da51020daa95f13eb78c8f898c5ec389550f229
Amy <amy.carney@alaska.gov>
2017-07-03 19:42:38 +0000

    update routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,10 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
+  
+  def signup
+  end
+  
+  def login
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,10 @@ module ApplicationHelper
   # Returns the full title on a per-page basis
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
-    base_title if page_title.empty?
-    page_title + " | " + base_title
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
   end
 end
