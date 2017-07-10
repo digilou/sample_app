390470801a839ea93d6bd288c52c9e39946ac869
Amy <amy.carney@alaska.gov>
2017-07-10 01:18:15 +0000

    Update basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,11 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
   
   # Returns the current logged-in user (if any).
   def current_user
@@ -14,4 +19,5 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+  
 end
